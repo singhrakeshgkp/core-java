aa311650355378d20db2ecbfad815d32b23ceb62
Rakesh singh <rakeshsingh@Rakeshs-MacBook-Pro.local>
2024-12-15 14:50:46 -0700

    added rotation.png file

---
+++ java-8proj/src/com/java/dsa/tree/RedBlackTree.java
@@ -0,0 +1,18 @@
+package com.java.dsa.tree;
+
+public class RedBlackTree {
+  class Node{
+    int data;
+    Node right;
+    Node left;
+    Node parent;
+    boolean color;
+
+    private void  rotateRight(Node node){
+      Node parent = node.parent;
+      Node leftChild = node.left;
+
+    }
+  }
+}
+
